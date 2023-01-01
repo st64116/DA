88d3c4e7e0df54091855b26c48cdf54854ea464a
st64116 <st64116@student.upce.cz>
2023-01-01 22:22:49 +0100

    nastaveni popup

--- app/views/_nastaveni.php
+++ app/views/_nastaveni.php
@@ -32,7 +32,24 @@ $userData = $db->view_zajemce($_SESSION['LOGIN']);
 
 ?>
 
-
+//popup
+<div class="modal fade" id="popup" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Pozor!</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Odebíráte si roly admina!!!
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+//formular
 <div class="shadow row text-center">
     <?php
     if (isset($errorMsg)) {
